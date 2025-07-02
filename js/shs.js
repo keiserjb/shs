0006d5e36af7c1c82064f9ea6c1fca1bda6128c4
Martin Price <github@systemhorizons.co.uk>
2025-07-02 19:54:35 +0100

    Rename json argument to avoid reserved word clash

--- js/shs.js
+++ js/shs.js
@@ -381,7 +381,7 @@
           var term = {
             vocabulary: settings.vocabulary,
             parent: (level === 1) ? 0 : ($triggering_element.prevAll('.shs-select').val() || 0),
-            name: termName
+            term_name: termName
           };
           if (termName.length > 0) {
             termAddNew($triggering_element, $container, term, base_id, level, settings);
